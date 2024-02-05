4215787d12026830f501fce85cae600c1474009c
Skrzypuu <kamil.skrzypek91@gmail.com>
2024-02-05 16:17:06 +0100

    BinaryCalculator

---
+++ js/app.js
@@ -0,0 +1,8 @@
+import BinaryCalculator from "./BinaryCalculator.js";
+import DecCalculator from "./DecCalculator.js";
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const bitCalc = new BinaryCalculator(".binary-calculator");
+    const decCalc = new DecCalculator(".dec-calculator");
+});
